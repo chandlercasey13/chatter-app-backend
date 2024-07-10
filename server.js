233ab0ed4002df3c367611a229433fc7e5708325
cbobak671 <cbobak.671@gmail.com>
2024-07-10 14:01:29 -0700

    Set up backend server, install socket.io and require  dependencies

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
-const dotenv = require('dotenv');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
+const server = createServer(app);
+const mongoose = require("mongoose");
+const testJWTRouter = require("./controllers/test-jwt");
+const usersRouter = require("./controllers/users");
+const profilesRouter = require("./controllers/profiles");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
 mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 app.use(cors());
 app.use(express.json());
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
+app.use("/test-jwt", testJWTRouter);
+app.use("/users", usersRouter);
+app.use("/profiles", profilesRouter);
 
-app.listen(3000, () => {
-    console.log('The express app is ready!');
+io.on("connection", (socket) => {});
+
+server.listen(3000, () => {
+  console.log(`Listening on ${3000}`);
 });
